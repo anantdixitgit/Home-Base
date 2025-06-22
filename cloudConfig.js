48997b40b8af96dd7f4df537aa9239e0d767de15
anant_dixit_13 <anantdixit100@gmail.com>
2025-06-22 20:54:31 +0530

    hehe

--- cloudConfig.js
+++ cloudConfig.js
@@ -4,9 +4,9 @@ const { CloudinaryStorage } = require("multer-storage-cloudinary");
 // const multer = require("multer");
 
 cloudinary.config({
-  cloud_name: process.env.cloud_NAME,
-  api_key: process.env.cloud_API_KEY,
-  api_secret: process.env.cloud_API_SECRET,
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
